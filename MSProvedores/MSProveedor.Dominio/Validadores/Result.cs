79cb0bb463f609a7fe0f4c65afe72210bfdec58b
Mateo Gandarillas <mateoganav@gmail.com>
2026-09-19 09:53:32 -0400

    refactor(msproveedor): make Result Data property nullable

--- MSProvedores/MSProveedor.Dominio/Validadores/Result.cs
+++ MSProvedores/MSProveedor.Dominio/Validadores/Result.cs
@@ -2,17 +2,24 @@ namespace MSProveedor.Dominio.Validadores;
 
 public class Result<T> : IResult<T>
 {
-    public bool Success { get; set; }
-    public string Message { get; set; } = string.Empty;
-    public T Data { get; set; }
+    public bool Success { get; }
+    public string Message { get; }
+    public T? Data { get; }
 
-    public static Result<T> Exito(T data, string message = "Operación exitosa")
+    private Result(bool success, string message, T? data)
     {
-        return new Result<T> { Success = true, Data = data, Message = message };
+        Success = success;
+        Message = message;
+        Data = data;
+    }
+
+    public static Result<T> Exito(T data, string message = "")
+    {
+        return new Result<T>(true, message, data);
     }
 
     public static Result<T> Falla(string message)
     {
-        return new Result<T> { Success = false, Data = default!, Message = message };
+        return new Result<T>(false, message, default);
     }
 }
